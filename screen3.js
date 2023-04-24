2f82a51537b8bdab5d9aa0fc3e34068c7939b0b1
nikiwiii <74990279+nikiwiii@users.noreply.github.com>
2023-04-24 12:18:20 +0200

    Update screen3.js

--- screen3.js
+++ screen3.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text,Image,View,StyleSheet } from 'react-native';
 
 class Screen3 extends React.Component {
 
     render(){
-        return(<Text>asas</Text>)
+        const img = require('./person.png')
+        return(
+          <View style={styles.centered}>
+            <Image source={img} style={styles.image}></Image>
+            <Text style={{color: 'gainsboro'}}>login:</Text>
+            <Text style={{color: 'white', fontSize: 26}}>{this.props.route.params.el.name}</Text>
+            <Text style={{color: 'gainsboro'}}>password:</Text>
+            <Text style={{color: 'white', fontSize: 26}}>{this.props.route.params.el.password}</Text>
+            <Text style={{color: 'gainsboro'}}>registered:</Text>
+            <Text style={{color: 'white', fontSize: 26}}>{this.props.route.params.el.registered}</Text>
+          </View>)
     }
 }
-
+const styles = StyleSheet.create({
+    centered: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        flexDirection: 'column',
+        marginBottom: 25,
+        marginTop: 25,
+        flex:.8
+    },
+    image: {
+        width: 160,
+        height: 160,
+        alignSelf: 'center'
+    },
+})
 export default Screen3;
